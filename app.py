9d73e162a06c2c0ff9e851bc88ed67b92015b58c
Zevin Leo <zevinn85@icloud.com>
2026-01-07 15:15:03 +0800

    更新版本号

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ class VideoProcessor:
 class UnifiedApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("智能视频筛选器            作者：倪梓纹")
+        self.root.title("智能视频筛选器V2.0           作者：倪梓纹")
         self.root.geometry("1400x950")
         
         self.current_filepath = None
